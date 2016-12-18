f787ac4c6bfab457c265e24a2d3386a7dd2c3a74
Tezza1 <tshenker@gmail.com>
2016-12-18 10:20:59 +0900

    Final version

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $("#hider").on("tap",function(){
+        $(this).hide();
+    });
+    
+    $("#index").on("swipe",function(){
+        $("#hider").hide();
+}   );
+
+    
+});
+
